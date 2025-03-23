752e8dd43b043c702318ce88ec19db9a1691ce60
ChongHauTien <chonghautian@gmail.com>
2025-03-24 00:29:47 +0800

    Display-Staff Dashboard
    Promotion Update  (x)
    Pop Up for Show More Pending Details(x)

--- AgriMarket/includes/database.php
+++ AgriMarket/includes/database.php
@@ -76,5 +76,68 @@ function getStaffList(){
         }
 }
 
+function getPendingRequestList(){
+    global $conn;
+
+    $sql="SELECT v.store_name, c.category_name,p.product_name, p.description, p.stock_quantity,p.weight, p.unit_price, p.product_id
+          FROM vendor v JOIN product p
+          ON v.vendor_id = p.vendor_id
+          JOIN category c 
+          ON p.category_id=c.category_id
+          WHERE p.product_status='Pending'";
+
+          $result=$conn ->query($sql);
+
+          if($result ->num_rows>0){
+            return $result-> fetch_all(MYSQLI_ASSOC);
+          }else{
+            return [];
+          }
+}
+
+function updatePendingRequestStatus($product_id,$status){
+        global $conn;
+
+        $sql="UPDATE product SET product_status=? WHERE product_id=?";
+
+        $stmt=$conn->prepare($sql);
+        $stmt->bind_param("si",$status,$product_id);
+
+        return $stmt->execute();
+}
+
+function getVendorAssisstanceList($user_id){
+    global $conn;
+
+    $sql="SELECT v.store_name,r.request_description, r.request_type,r.request_date, r.request_id
+          FROM request r JOIN vendor v
+          ON r.vendor_id = v.vendor_id
+          WHERE v.staff_assisstance_id=? AND r.is_completed=FALSE";
+
+    $stmt =$conn->prepare($sql);
+    $stmt ->bind_param("i",$user_id);
+    $stmt ->execute();
+
+    $result= $stmt->get_result();
+    return $result->fetch_all(MYSQLI_ASSOC);
+}
+
+function updateAssisstanceRequestStatus($request_id,$status){
+    global $conn;
+
+    $sql="UPDATE request SET is_completed=? WHERE request_id= ?";
+    $stmt= $conn->prepare($sql);
+
+    //Convert Boolean to Integer 
+    $status =(int)$status;
+
+    $stmt->bind_param("ii",$status,$request_id);
+
+    if($stmt->execute()){
+        return true;
+    }else {
+        return false;
+    }
+}
 
 ?>
