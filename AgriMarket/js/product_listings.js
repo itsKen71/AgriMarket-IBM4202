bb2b83a8a11242376df548f965a5ec1cd2cf9926
LeBonBon-TheCreator <shyang04@gmail.com>
2025-03-30 16:20:25 +0800

    Product_listing
    display product, edit product

---
+++ AgriMarket/js/product_listings.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const editButtons = document.querySelectorAll(".edit-btn");
+
+    editButtons.forEach(button => {
+        button.addEventListener("click", function () {
+            document.getElementById("editProductId").value = this.dataset.id;
+            document.getElementById("editProductName").value = this.dataset.name;
+            document.getElementById("editDescription").value = this.dataset.description;
+            document.getElementById("editStock").value = this.dataset.stock;
+            document.getElementById("editWeight").value = this.dataset.weight;
+            document.getElementById("editPackaging").value = this.dataset.packaging;
+            document.getElementById("editPrice").value = this.dataset.price;
+        });
+    });
+});
